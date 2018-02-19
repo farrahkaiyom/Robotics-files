654af894b7740f4ef0422272490b456f08e88703
farrahkaiyom <farrah.kaiyom@gmail.com>
2018-02-18 16:19:49 -0800

    project 5

---
+++ FrcProject_05/src/torg/usfirst/frc/team3647/robot/Motors.java
@@ -0,0 +1,10 @@
+package torg.usfirst.frc.team3647.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Motors 
+{
+	public static Spark leftMotor = new Spark(2);
+	public static Spark rightMotor = new Spark(1);
+}
+
